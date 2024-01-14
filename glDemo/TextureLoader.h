56ea3db0cbf0baa0fe5064ddbafaa8e48b148956
Mario Battiston <146736133+MarioBatt38@users.noreply.github.com>
2024-01-14 16:12:10 +0000

    Setting Up scene for magos

--- glDemo/TextureLoader.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "core.h"
-
-// Helper function for loading texture images from disk and setup a texture with defaut properties
-GLuint loadTexture(std::string filename, FREE_IMAGE_FORMAT srcImageType);
